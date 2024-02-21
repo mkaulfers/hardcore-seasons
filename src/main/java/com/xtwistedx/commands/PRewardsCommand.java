65f9c39306c460416a96fd2218072f45cb13f5d1
mkaulfers <matthew.kaulfers@yahoo.com>
2024-02-21 16:51:18 -0600

    Add various configuration files and settings for project setup. Set up ignored files, compiler configurations, encodings, jar repositories, project modules, and UI designer components.

---
+++ src/main/java/com/xtwistedx/commands/PRewardsCommand.java
@@ -0,0 +1,13 @@
+package com.xtwistedx.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class PRewardsCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+        return false;
+    }
+}
