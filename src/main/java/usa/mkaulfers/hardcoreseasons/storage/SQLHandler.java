7d92e4c2ec6dc5934f8dc1f48e2495f11459774d
mkaulfers <matthew.kaulfers@yahoo.com>
2024-02-23 11:22:00 -0600

    Add HikariCP dependency, update database schema creation.
    The commit adds the HikariCP dependency to the project and updates the SQLHandler class to use a connection pool with HikariDataSource for better performance. It also refactors the database schema creation queries to ensure tables are created if they do not exist.

--- src/main/java/usa/mkaulfers/hardcoreseasons/storage/SQLHandler.java
+++ src/main/java/usa/mkaulfers/hardcoreseasons/storage/SQLHandler.java
@@ -1,21 +1,21 @@
 package usa.mkaulfers.hardcoreseasons.storage;
 
+import com.zaxxer.hikari.HikariDataSource;
 import usa.mkaulfers.hardcoreseasons.models.Config;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class SQLHandler {
     private Config config;
-    private Connection connection;
+    private HikariDataSource dataSource;
 
     public boolean isConnected() {
-        return connection != null;
+        return dataSource != null;
     }
 
-    public void connect() throws SQLException {
+    public void connect() {
         String host = config.mySQLConfig.host;
         int port = config.mySQLConfig.port;
         String database = config.mySQLConfig.database;
@@ -23,39 +23,34 @@ public class SQLHandler {
         String username = config.mySQLConfig.username;
         String password = config.mySQLConfig.password;
 
-        String JDBC_URL = "jdbc:mysql://" + host + ":" + port + "/" + database + "?useSSL=false";
+        dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:mysql://" + host + ":" + port + "/" + database);
+        dataSource.addDataSourceProperty("user", username);
+        dataSource.addDataSourceProperty("password", password);
 
-        connection = DriverManager.getConnection(JDBC_URL, username, password);
         initializeDatabase();
     }
 
     public void disconnect() {
         if (isConnected()) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+            dataSource.close();
         }
     }
 
-    public void initializeDatabase() throws SQLException {
+    public void initializeDatabase() {
         if (!isConnected()) {
             throw new IllegalStateException("Database is not connected");
         }
 
-        // Create schema
-        String CREATE_SCHEMA_QUERY = "CREATE SCHEMA IF NOT EXISTS `" + config.mySQLConfig.database + "`";
-        Statement stmt = connection.createStatement();
-        stmt.execute(CREATE_SCHEMA_QUERY);
+        try (Connection connection = dataSource.getConnection()) {
+            Statement stmt = connection.createStatement();
 
-        // Switch to created schema
-        String USE_SCHEMA_QUERY = "USE `" + config.mySQLConfig.database + "`";
-        stmt.execute(USE_SCHEMA_QUERY);
+            String USE_SCHEMA_QUERY = "USE `" + config.mySQLConfig.database + "`";
+            stmt.execute(USE_SCHEMA_QUERY);
 
-        // Separate strings to create each table independently
-        String CREATE_SEASONS_TABLE_QUERY = """
-                CREATE TABLE `Seasons` (
+            // Separate strings to create each table independently
+            String CREATE_SEASONS_TABLE_QUERY = """
+                CREATE TABLE IF NOT EXISTS  `seasons` (
                   `season_id` INT AUTO_INCREMENT,
                   `start_date` DATETIME,
                   `end_date` DATETIME,
@@ -63,42 +58,67 @@ public class SQLHandler {
                   PRIMARY KEY (`season_id`)
                 );
                 """;
-        stmt.execute(CREATE_SEASONS_TABLE_QUERY);
+            stmt.execute(CREATE_SEASONS_TABLE_QUERY);
 
-        String CREATE_PLAYERS_TABLE_QUERY = """
-                CREATE TABLE `Players` (
+            String CREATE_PLAYERS_TABLE_QUERY = """
+                CREATE TABLE IF NOT EXISTS  `player_details` (
                   `player_id` INT AUTO_INCREMENT,
                   `first_joined_date` DATETIME,
                   `last_joined_date` DATETIME,
                   PRIMARY KEY (`player_id`)
                 );
                 """;
-        stmt.execute(CREATE_PLAYERS_TABLE_QUERY);
+            stmt.execute(CREATE_PLAYERS_TABLE_QUERY);
 
-        String CREATE_SEASONS_PLAYERS_TABLE_QUERY = """
-                CREATE TABLE `Seasons_Players` (
+            String CREATE_SEASONS_PLAYERS_TABLE_QUERY = """
+                CREATE TABLE IF NOT EXISTS  `season_participants` (
                   `season_player_id` INT AUTO_INCREMENT,
                   `season_id` INT,
                   `player_id` INT,
                   PRIMARY KEY (`season_player_id`),
-                  FOREIGN KEY (`season_id`) REFERENCES `Seasons`(`season_id`),
-                  FOREIGN KEY (`player_id`) REFERENCES `Players`(`player_id`)
+                  FOREIGN KEY (`season_id`) REFERENCES `seasons`(`season_id`),
+                  FOREIGN KEY (`player_id`) REFERENCES `player_details`(`player_id`)
                 );
                 """;
-        stmt.execute(CREATE_SEASONS_PLAYERS_TABLE_QUERY);
+            stmt.execute(CREATE_SEASONS_PLAYERS_TABLE_QUERY);
 
-        String CREATE_CONTAINERS_TABLE_QUERY = """
-                CREATE TABLE `Containers` (
+            String CREATE_CONTAINERS_TABLE_QUERY = """
+                CREATE TABLE IF NOT EXISTS `tracked_containers` (
                   `container_id` INT AUTO_INCREMENT,
                   `player_id` INT,
                   `season_id` INT,
                   `inventory_data` BLOB,
                   PRIMARY KEY (`container_id`),
-                  FOREIGN KEY (`player_id`) REFERENCES `Players`(`player_id`),
-                  FOREIGN KEY (`season_id`) REFERENCES `Seasons`(`season_id`)
+                  FOREIGN KEY (`player_id`) REFERENCES `player_details`(`player_id`),
+                  FOREIGN KEY (`season_id`) REFERENCES `seasons`(`season_id`)
                 );
                 """;
-        stmt.execute(CREATE_CONTAINERS_TABLE_QUERY);
+            stmt.execute(CREATE_CONTAINERS_TABLE_QUERY);
+
+            String CREATE_PLAYER_INVENTORIES_TABLE_QUERY = """
+                CREATE TABLE IF NOT EXISTS `player_inventories` (
+                  `inventory_id` INT AUTO_INCREMENT,
+                  `player_id` INT,
+                  `inventory_data` BLOB,
+                  PRIMARY KEY (`inventory_id`),
+                  FOREIGN KEY (`player_id`) REFERENCES `player_details`(`player_id`)
+                );
+                """;
+            stmt.execute(CREATE_PLAYER_INVENTORIES_TABLE_QUERY);
+
+            String CREATE_PLAYER_END_CHEST_INVENTORIES_TABLE_QUERY = """
+                CREATE TABLE IF NOT EXISTS `player_end_chest_inventories` (
+                  `inventory_id` INT AUTO_INCREMENT,
+                  `player_id` INT,
+                  `inventory_data` BLOB,
+                  PRIMARY KEY (`inventory_id`),
+                  FOREIGN KEY (`player_id`) REFERENCES `player_details`(`player_id`)
+                );
+                """;
+            stmt.execute(CREATE_PLAYER_END_CHEST_INVENTORIES_TABLE_QUERY);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public SQLHandler(Config config) {
