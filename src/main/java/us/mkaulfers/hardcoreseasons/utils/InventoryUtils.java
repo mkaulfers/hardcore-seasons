19fcec8a14cff7fabce88d945b15ff9f756530f1
mkaulfers <matthew.kaulfers@yahoo.com>
2024-03-01 12:46:21 -0600

    Refactor inventory serialization methods
    Added methods to convert ItemStack to/from Base64 strings for easier storage and retrieval.

--- src/main/java/us/mkaulfers/hardcoreseasons/utils/InventoryUtils.java
+++ src/main/java/us/mkaulfers/hardcoreseasons/utils/InventoryUtils.java
@@ -86,5 +86,51 @@ public class InventoryUtils {
             throw new IOException("Unable to decode class type.", e);
         }
     }
+
+    /**
+     * Gets one {@link ItemStack} from Base64 string.
+     *
+     * @param data Base64 string to convert to {@link ItemStack}.
+     * @return {@link ItemStack} created from the Base64 string.
+     * @throws IOException
+     */
+    public static ItemStack itemStackFromBase64(String data) throws IOException {
+        try {
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decode(data));
+            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
+            ItemStack item;
+
+            // Read the serialized inventory
+            item = (ItemStack) dataInput.readObject();
+
+            dataInput.close();
+            return item;
+        } catch (ClassNotFoundException e) {
+            throw new IOException("Unable to decode class type.", e);
+        }
+    }
+
+    /**
+     * A method to serialize one {@link ItemStack} to Base64 String.
+     *
+     * @param item to turn into a Base64 String.
+     * @return Base64 string of the item.
+     * @throws IllegalStateException
+     */
+    public static String itemStackToBase64(ItemStack item) throws IllegalStateException {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+
+            // Save every element
+            dataOutput.writeObject(item);
+
+            // Serialize that array
+            dataOutput.close();
+            return new String(Base64Coder.encode(outputStream.toByteArray()));
+        } catch (Exception e) {
+            throw new IllegalStateException("Unable to save item stacks.", e);
+        }
+    }
 }
 
